c8081404b5bdec5e8b72a69b045153e4b75eef9e
ryan <ryanjboyer@gmail.com>
2019-12-03 17:25:05 -0700

    Updated with UV setter

--- AnimationTextureCreator.cs
+++ AnimationTextureCreator.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using Unity.EditorCoroutines;
 
 public class AnimationTextureCreator : EditorWindow
 {
+    private readonly string[] TabTitles = new string[] {
+        "Animation Texture",
+        "UV Map"
+    };
+    private int currentTab;
+
+    #region Animation Texture Props
     private GameObject riggedPrefab;
-    private string errorMessage;
-    private List<SelectedChild> finalChildren;
-    private bool finalChildFoldout;
-    private List<SelectedChild> selectedFinalChildren;
     private int framesPerSecondCapture = 60;
     private float scaler = 0.5f;
+    #endregion
+
+    #region UV Map Props
+    private Texture2D animationTexture;
+    private GameObject meshContainer;
+    private UVLayer uvLayer;
+    private RendererType rendererType;
+    #endregion
 
     private Vector2 scrollPos = Vector2.zero;
 
@@ -24,22 +36,43 @@ public class AnimationTextureCreator : EditorWindow
 
     void OnGUI()
     {
+        currentTab = GUILayout.Toolbar(currentTab, TabTitles);
+
+        switch (currentTab)
+        {
+            case 0:
+                AnimationTextureEditor();
+                break;
+            case 1:
+                UVMapEditor();
+                break;
+        }
+    }
+
+    #region Animation Texture
+    void AnimationTextureEditor()
+    {
         riggedPrefab = (GameObject)EditorGUILayout.ObjectField("Rigged Prefab", riggedPrefab, typeof(GameObject), true);
+        rendererType = (RendererType)EditorGUILayout.EnumPopup("Renderer Type", rendererType);
 
         if (riggedPrefab == null)
         {
             return;
         }
-        errorMessage = null;
-        if (riggedPrefab.GetComponentInChildren<SkinnedMeshRenderer>() == null)
+        string errorMessage = null;
+        if (rendererType == RendererType.Skinned && riggedPrefab.GetComponentInChildren<SkinnedMeshRenderer>() == null)
         {
-            errorMessage = "ERROR: Could not find a Skinned Mesh Renderer\nin the object's children.";
+            errorMessage = "ERROR: Could not find a Skinned Mesh Renderer\nin the object's hierarchy.";
+        }
+        if (rendererType == RendererType.Normal && riggedPrefab.GetComponentInChildren<MeshFilter>() == null)
+        {
+            errorMessage = "ERROR: Could not find a Mesh Filter\nin the object's hierarchy.";
         }
         if (riggedPrefab.GetComponentInChildren<Animator>() == null)
         {
-            errorMessage = "ERROR: Could not find an Animator\nin the object's children.";
+            errorMessage = "ERROR: Could not find an Animator\nin the object's hierarchy.";
         }
-        if (riggedPrefab.GetComponentInChildren<Animator>().runtimeAnimatorController == null)
+        if (riggedPrefab.GetComponentInChildren<Animator>()?.runtimeAnimatorController == null)
         {
             errorMessage = "ERROR: Could not find a Runtime Animator Controller\nin the Animator's settings.";
         }
@@ -55,29 +88,6 @@ public class AnimationTextureCreator : EditorWindow
         }
 
         scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
-
-        if (finalChildren == null || finalChildren.Count == 0)
-        {
-            finalChildren = new List<SelectedChild>();
-            GetFinalChild(riggedPrefab.transform, finalChildren);
-        }
-
-        finalChildFoldout = EditorGUILayout.Foldout(finalChildFoldout, "Final Children");
-        if (finalChildFoldout)
-        {
-            if (selectedFinalChildren == null)
-            {
-                selectedFinalChildren = new List<SelectedChild>();
-            }
-
-            for (int i = 0; i < finalChildren.Count; i++)
-            {
-                EditorGUILayout.BeginHorizontal();
-                EditorGUILayout.LabelField(finalChildren[i].child.name);
-                finalChildren[i].selected = EditorGUILayout.Toggle("", finalChildren[i].selected);
-                EditorGUILayout.EndHorizontal();
-            }
-        }
 
         framesPerSecondCapture = EditorGUILayout.IntSlider("FPS Capture", framesPerSecondCapture, 24, 120);
         scaler = EditorGUILayout.FloatField("Scaler", scaler);
@@ -97,33 +107,14 @@ public class AnimationTextureCreator : EditorWindow
             EditorGUILayout.LabelField("Resulting texture size: " + textureSize + "x" + textureSize);
             if (GUILayout.Button("Create Animation Texture"))
             {
-                CreateAnimationTexture(textureSize, vertexCount, totalFrames, framesPerSecondCapture, animationClips[0]);
+                Unity.EditorCoroutines.Editor.EditorCoroutineUtility.StartCoroutine(CreateAnimationTexture(textureSize, vertexCount, totalFrames, framesPerSecondCapture, animationClips[0]), this);
             }
         }
 
         EditorGUILayout.EndScrollView();
     }
 
-    void GetFinalChild(Transform child, List<SelectedChild> finalChildren)
-    {
-        if (child.childCount == 0)
-        {
-            finalChildren.Add(new SelectedChild
-            {
-                selected = false,
-                child = child
-            });
-        }
-        else
-        {
-            for (int i = 0; i < child.childCount; i++)
-            {
-                GetFinalChild(child.GetChild(i), finalChildren);
-            }
-        }
-    }
-
-    void CreateAnimationTexture(int size, int vertexCount, int frames, int fps, AnimationClip clip)
+    IEnumerator CreateAnimationTexture(int size, int vertexCount, int frames, int fps, AnimationClip clip)
     {
         GameObject prefab = Instantiate(riggedPrefab);
         Animator animator = prefab.GetComponentInChildren<Animator>();
@@ -132,7 +123,6 @@ public class AnimationTextureCreator : EditorWindow
 
         Texture2D result = new Texture2D(size, size, UnityEngine.Experimental.Rendering.DefaultFormat.LDR, UnityEngine.Experimental.Rendering.TextureCreationFlags.None);
 
-        animator.Play(clip.name, 0);
 
         Color[] clearColors = new Color[size * size];
         for (int i = 0; i < clearColors.Length; i++)
@@ -141,10 +131,19 @@ public class AnimationTextureCreator : EditorWindow
         }
         result.SetPixels(clearColors);
 
+        animator.Play(clip.name, 0, 0);
+        yield return null;
+        animator.Update(0);
+
+        float animationDeltaTime = 1f / fps;
+
         int y = 0;
         int x = 0;
         for (int i = 0; i < frames; i++)
         {
+            Mesh meshFrame = new Mesh();
+            skinnedMesh.BakeMesh(meshFrame);
+
             //red = x
             //green = y
             //blue = z
@@ -152,11 +151,18 @@ public class AnimationTextureCreator : EditorWindow
             for (int j = 0; j < vertexCount; j++)
             {
                 Color pixel = Color.clear;
-                Vector3 position = skinnedMesh.sharedMesh.vertices[j] * scaler;
+                Vector3 position = (meshFrame.vertices[j] * scaler) + (Vector3.one * 0.5f);
                 pixel.r = position.x;
                 pixel.g = position.y;
                 pixel.b = position.z;
                 pixel.a = 1;
+
+                if (position.x > 1 || position.x < 0
+                || position.y > 1 || position.y < 0
+                || position.z > 1 || position.z < 0)
+                {
+                    Debug.LogWarning("A pixel was out of range!  It's clamped, but it won't appear correctly");
+                }
 
                 result.SetPixel(x, y, pixel);
 
@@ -168,7 +174,14 @@ public class AnimationTextureCreator : EditorWindow
                 }
             }
 
-            animator.Update(1f / fps);
+            DestroyImmediate(meshFrame);
+
+            animator.Update(animationDeltaTime);
+
+            for (int j = 0; j < 10; j++)
+            {
+                yield return null;
+            }
         }
 
         DestroyImmediate(prefab);
@@ -183,10 +196,103 @@ public class AnimationTextureCreator : EditorWindow
         AssetDatabase.ImportAsset(string.Concat("Assets/", riggedPrefab.name, ".png"));
         DestroyImmediate(result);
     }
+    #endregion
 
-    public class SelectedChild
+    #region UV Map
+    void UVMapEditor()
     {
-        public bool selected;
-        public Transform child;
+        animationTexture = (Texture2D)EditorGUILayout.ObjectField("Animation Texture", animationTexture, typeof(Texture2D), false);
+        meshContainer = (GameObject)EditorGUILayout.ObjectField("Mesh Container", meshContainer, typeof(GameObject), true);
+        uvLayer = (UVLayer)EditorGUILayout.EnumPopup("UV Layer", uvLayer);
+        rendererType = (RendererType)EditorGUILayout.EnumPopup("Renderer Type", rendererType);
+
+        bool sendUVError = false;
+
+        Mesh mesh = null;
+        if (meshContainer != null)
+        {
+            if (rendererType == RendererType.Normal && meshContainer.GetComponentInChildren<MeshFilter>() != null)
+            {
+                mesh = meshContainer.GetComponentInChildren<MeshFilter>().sharedMesh;
+            }
+            else if (meshContainer.GetComponentInChildren<SkinnedMeshRenderer>() != null)
+            {
+                mesh = meshContainer.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh;
+            }
+            if (mesh != null)
+            {
+                List<Vector2> uvList = new List<Vector2>();
+                mesh.GetUVs((int)uvLayer, uvList);
+                sendUVError = uvList.Count > 0;
+            }
+        }
+
+        if (sendUVError)
+        {
+            EditorGUILayout.LabelField("Warning: This mesh already has a UV on this layer.");
+            EditorGUILayout.LabelField("Are you sure you want to overwrite it?");
+        }
+
+        if (mesh != null)
+        {
+            EditorGUILayout.Space(16);
+            if (GUILayout.Button("Set UV For Layer"))
+            {
+                ApplyUVToLayer(mesh);
+            }
+        }
     }
+
+    void ApplyUVToLayer(Mesh originalMesh)
+    {
+        float gridInterval = 1f / animationTexture.height;
+        Vector2 initialOffset = Vector2.one * gridInterval * 0.5f;
+
+        Vector2[] resultUV = new Vector2[originalMesh.vertexCount];
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < resultUV.Length; i++)
+        {
+            Vector2 gridPosition = new Vector2(x, y) * gridInterval;
+            resultUV[i] = gridPosition + initialOffset;
+
+            y++;
+            if (y == animationTexture.height)
+            {
+                x++;
+                y = 0;
+            }
+        }
+
+        originalMesh.SetUVs((int)uvLayer, resultUV);
+        Mesh instantiation = Instantiate(originalMesh);
+        instantiation.name = originalMesh.name;
+        if (rendererType == RendererType.Normal)
+        {
+            meshContainer.GetComponentInChildren<MeshFilter>().sharedMesh = instantiation;
+        }
+        else
+        {
+            meshContainer.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh = instantiation;
+        }
+    }
+
+    public enum RendererType
+    {
+        Skinned,
+        Normal,
+    }
+
+    public enum UVLayer
+    {
+        UV0,
+        UV1,
+        UV2,
+        UV3,
+        UV4,
+        UV5,
+        UV6,
+        UV7,
+    }
+    #endregion
 }
